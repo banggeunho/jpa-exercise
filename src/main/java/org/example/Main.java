b7263543e79202197021efe1918e01e9d6ddf822
banggeunho <panggeunho@gmail.com>
2023-07-03 21:26:42 +0900

    EntityManager 실습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = em.find(Member.class, 2L);
+            List<Member> memberList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1) // offset
+                    .setMaxResults(10) // limit
+                    .getResultList();
+
+            for (Member member : memberList) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+//            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
 }
